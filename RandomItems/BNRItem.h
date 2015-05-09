8fd159d1fc4c0ee201f8f335ef80cbef38ba04fd
Kris Kata <kata.krzysztof@gmail.com>
2015-05-09 01:44:25 -0500

    created BNRItem

---
+++ RandomItems/BNRItem.h
@@ -0,0 +1,29 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Kris Kata on 5/9/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+@end
---
+++ RandomItems/BNRItem.m
@@ -0,0 +1,48 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Kris Kata on 5/9/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+- (void)setItemName:(NSString *)str
+{
+    _itemName = str;
+}
+
+- (NSString *)itemName
+{
+    return _itemName;
+}
+
+- (void)setSerialNumber:(NSString *)str
+{
+    _serialNumber = str;
+}
+
+- (NSString *)serialNumber
+{
+    return _serialNumber;
+}
+
+- (void)setValueInDollars:(int)v
+{
+    _valueInDollars = v;
+}
+
+- (int)valueInDollars
+{
+    return _valueInDollars;
+}
+
+- (NSDate *)dateCreated
+{
+    return _dateCreated;
+}
+
+@end
