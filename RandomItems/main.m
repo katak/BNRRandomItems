0bbbea9f5c75755ea175e2cdcc1ef0d863b35138
Kris Kata <kata.krzysztof@gmail.com>
2015-05-09 01:10:17 -0500

    initial project commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Kris Kata on 5/9/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
