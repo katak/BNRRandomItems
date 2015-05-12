48fa7edde783656590a47b9eeba7228236f24c84
Kris Kata <kata.krzysztof@gmail.com>
2015-05-11 19:55:12 -0700

    More initializers for BNRItem, class method for random item creation

--- RandomItems/BNRItem.h
+++ RandomItems/BNRItem.h
@@ -16,6 +16,15 @@
     NSDate *_dateCreated;
 }
 
++ (instancetype)randomItem;
+
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
 - (void)setItemName:(NSString *)str;
 - (NSString *)itemName;
 
--- RandomItems/main.m
+++ RandomItems/main.m
@@ -17,34 +17,14 @@ int main(int argc, const char * argv[])
         // Create a mutable array object, store its address in items variable
         NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        // Send the message addObject: to the NSMutableArray pointed to
-        // by the variable items, passing a string each time
-        [items addObject:@"One"];
-        [items addObject:@"Two"];
-        [items addObject:@"Three"];
-        
-        // Send another message, insertObject:atIndex:, to that same array object
-        [items insertObject:@"Zero" atIndex:0];
-        
-        // For every item in the items array...
-        for(NSString *item in items){
-            // Log the description of item
-            NSLog(@"%@", item);
+        for (int i = 0; i < 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
         }
         
-        BNRItem *item = [[BNRItem alloc] init];
-        
-        // This creates an NSString, "Red Sofa" and gives it to the BNRItem
-        item.itemName = @"Red Sofa";
-        
-        // This creates an NSString, "A1B2C" and gives it to the BNRItem
-        item.serialNumber = @"A1B2C";
-        
-        // This sends the value 100 to be used as the valueInDollars of this BNRItem
-        item.valueInDollars = 100;
-        
-        NSLog(@"%@ %@ %@ %d", item.itemName, item.dateCreated,
-                              item.serialNumber, item.valueInDollars);
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
         
         // Destroy the mutable array object
         items = nil;
